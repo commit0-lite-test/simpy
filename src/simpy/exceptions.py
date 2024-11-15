d5742ee75ce58e414a9089c7918cc1c083091286
nanjiangwill <willjiang2018@gmail.com>
2024-11-15 12:38:21 -0800

    fix: Fix formatting and add type annotations to SimPy exceptions

--- src/simpy/exceptions.py
+++ src/simpy/exceptions.py
@@ -2,11 +2,15 @@
 SimPy specific exceptions.
 
 """
+
 from __future__ import annotations
+
 from typing import Any, Optional
+
 
 class SimPyException(Exception):
     """Base class for all SimPy specific exceptions."""
+
 
 class Interrupt(SimPyException):
     """Exception thrown into a process if it is interrupted (see
